6f03d561d0d799fefba50ca8f6a2a037a3535434
Erik Gomez <e@erikgomez.co>
2019-11-24 18:40:06 -0800

    Add body-parser, create POST endpoints, read data from req.body

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { buildSchema } from 'type-graphql';
 import { HelloWorldResolver } from './resolvers/HelloWorldResolver';
 import { MovieResolver } from './resolvers/MovieResolver';
 import chalk from 'chalk';
+import bodyParser from 'body-parser';
 
 (async () => {
   const app = express();
@@ -19,46 +20,52 @@ import chalk from 'chalk';
     context: ({ req, res }) => ({ req, res })
   });
 
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(bodyParser.json());
+
   apolloServer.applyMiddleware({ app, cors: false });
 
-  app.listen(4000, () => {
-    console.log(chalk.green.bold('READY'));
-  });
-
-  app.get('/create', async (req, res) => {
-    if (!req.query.title || !req.query.minutes) {
-      res.json({ error: 'Mising Fields. Make sure TITLE and MINUTES are set' });
+  app.post('/create', async (req, res) => {
+    if (!req.body.title || !req.body.minutes) {
+      return res.status(500).send({
+        error: 'Mising Fields. Make sure TITLE and MINUTES are set'
+      });
     }
 
     const resolver = new MovieResolver();
-    const title = req.query.title;
-    const minutes = req.query.minutes;
-    const firstName = req.query.firstName || null;
-    const lastName = req.query.lastName || null;
+    const title = req.body.title;
+    const minutes = req.body.minutes;
+    const firstName = req.body.firstName || null;
+    const lastName = req.body.lastName || null;
 
-    await resolver
+    return await resolver
       .createMovie({ title, minutes, firstName, lastName })
       .then(movie => res.json({ movie }))
       .catch(error => res.json({ error }));
   });
 
-  app.get('/update', async (req, res) => {
-    if (!req.query.id && !req.query.firstName && !req.query.lastName) {
-      res.json({
+  app.post('/update', async (req, res) => {
+    if (!req.body.id && !req.body.firstName && !req.body.lastName) {
+      return res.status(500).send({
         error: 'Missing fields. Make sure ID, FIRST NAME and LAST NAME are set'
       });
     }
 
     const resolver = new MovieResolver();
-    const firstName = req.query.firstName;
-    const lastName = req.query.lastName;
-    const id = req.query.id;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const id = req.body.id;
 
-    await resolver
+    return await resolver
       .findMovieById(id)
       .then(async () =>
         res.json(await resolver.updateMovie(id, { firstName, lastName }))
       )
       .catch(error => res.json({ error }));
+  });
+
+  app.listen(4000, () => {
+    console.log(chalk.green.bold('READY'));
   });
 })();
